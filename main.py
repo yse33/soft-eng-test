93d99006b4a9be228d62bc8b04b00971102f0f3e
yse33 <yse33@abv.bg>
2024-03-22 09:31:10 +0200

    main.py blueprint

---
+++ main.py
@@ -0,0 +1,31 @@
+def even_numbers(n):
+
+
+def odd_numbers(n):
+
+
+def print_menu():
+    print("1. Odd numbers")
+    print("2. Even numbers")
+    print("3. Fibonacci sequence")
+    print("4. Prime numbers")
+
+
+def main():
+    print_menu()
+    function = int(input("Your choice: "))
+    maxNum = int(input("Enter max number: "))
+    if function == 1:
+        pass
+    if function == 2:
+        pass
+    if function == 3:
+        pass
+    if function == 4:
+        pass
+    else:
+        print("Invalid choice")
+
+
+if __name__ == '__main__':
+    main()
