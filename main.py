3cf0ba8bd2784f0b5bbd6d3f5317451320a65236
yse33 <yse33@abv.bg>
2024-03-22 09:51:08 +0200

    s

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def even_numbers(n):
 def odd_numbers(n):
     #implement
 
+
+def prime_numbers(n):
+    # implementation
+
 def print_menu():
     print("1. Odd numbers")
     print("2. Even numbers")
@@ -20,6 +24,8 @@ def main():
         odd_numbers(maxNum)
     elif function == 2:
         even_numbers(maxNum)
+    elif function == 4:
+        prime_numbers(maxNum)
     else:
         print("Invalid choice")
 
